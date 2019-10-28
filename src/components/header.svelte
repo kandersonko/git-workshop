b06fab395960d52643954729353ef0f6f1510f51
kandersonko <kandersonko@gmail.com>
2019-10-28 02:03:47 -0700

    Add header

---
+++ src/components/header.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let title;
+</script>
+
+<header
+  class="flex justify-between bg-blue-600 text-indigo-100 px-6 py-3 shadow-lg
+  mb-4">
+  <h1 class="text-xl font-semibold">{title}</h1>
+</header>
