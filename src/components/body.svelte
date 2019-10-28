20f6dc208f022e4a65e0da0ca17fee68cb89d7f5
Koffi, Koffi Anderson <kandersonko@gmail.com>
2019-10-28 16:20:38 -0700

    Fix page title

--- src/components/body.svelte
+++ src/components/body.svelte
@@ -5,7 +5,9 @@
 <div
   class="container mx-auto px-2 md:px-4 py-2 flex-1 flex flex-col items-center
   min-h-0">
-  <h1 class="text-center text-3xl text-gray-700 mb-6">List of attendees</h1>
+  <h1 class="text-center text-3xl text-gray-700 mb-6">
+    Git Workshop attendees
+  </h1>
 
   <Attendees />
 
